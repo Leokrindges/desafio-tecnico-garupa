8e6234edc436ff1047aef0b43a44b8a8df676753
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-07-27 00:17:52 -0300

    feature/ Corrigido validação da data de validade usando a biblioteca moment

--- src/middlewares/transfers/transfers.middleware.ts
+++ src/middlewares/transfers/transfers.middleware.ts
@@ -1,31 +1,37 @@
+import { log } from "console";
 import { NextFunction, Request, Response } from "express";
+import moment from "moment";
 
 export class CreateTransfersMiddleware {
   public static validate(req: Request, res: Response, next: NextFunction) {
-    const { externalID, amount, expectedOn, type } = req.body;
+    const { externalId, amount, expectedOn, type } = req.body;
 
-    if (!type || type !== "Entrada" || type !== "Saida") {
+    if (!type || (type !== "Entrada" && type !== "Saida")) {
       return res.status(400).json({
         ok: false,
         message: "Tipo da transação inválida!",
       });
     }
+    const dateFormated = moment(expectedOn, "DD/MM/YYYY");
 
-    if (expectedOn && new Date(expectedOn) < new Date()) {
+    if (
+      expectedOn &&
+      new Date(dateFormated.format("YYYY-MM-DD")) < new Date()
+    ) {
       return res.status(400).json({
         ok: false,
-        message: "Data de vencimento menor que a data atual",
+        message: "Data de vencimento é menor que a data atual",
       });
     }
 
-    if (!amount || amount <= 0) {
+    if (!amount || amount <= 0 || typeof amount !== "number") {
       return res.status(400).json({
         ok: false,
         message: "Valor da transferência é obrigatório",
       });
     }
 
-    if (!externalID || typeof externalID !== "string") {
+    if (!externalId) {
       return res.status(400).json({
         ok: false,
         message: "Id externo é obrigatório!",
