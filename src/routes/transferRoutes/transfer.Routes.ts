5b011e1b2a0e551c816993cdc98cf03a19bb8252
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-07-27 09:51:36 -0300

    feature/ Adicionado middleware de validacao do uuid, e chamado a nova rota para listar todos as transferencias

--- src/routes/transferRoutes/transfer.Routes.ts
+++ src/routes/transferRoutes/transfer.Routes.ts
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import { CreateTransfersMiddleware } from "../../middlewares/transfers/transfers.middleware";
 import { TransferController } from "../../controllers/transfer/transfer.controller";
+import { ValidUuidParamsMiddleware } from "../../middlewares/common/Valid-uui-paramans.middlaware";
 
 export class TransfersRouts {
   public static execute(): Router {
     const router = Router();
 
-    router.post("/", [CreateTransfersMiddleware.validate], TransferController.create)
+    router.post(
+      "/",
+      [CreateTransfersMiddleware.validate],
+      TransferController.create
+    );
+    router.get(
+      "/",
+      [ValidUuidParamsMiddleware.validate],
+      TransferController.listAl
+    );
 
-    return router
+    return router;
   }
 }
